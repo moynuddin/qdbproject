d48e10faed70b0cbe75d80adf0e383972a166d06
Moin uddin <moin_uddin@mohawkind.com>
2023-06-01 00:38:45 +0530

    added .env

--- src/utility/api.ts
+++ src/utility/api.ts
@@ -1,28 +1,26 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const baseURL = "https://jsonplaceholder.typicode.com/users/1";
-
 // import.meta.env.VITE_URI ||
 
 export const getPosts = async (): Promise<unknown> => {
-  const data = await axios.get(`${baseURL}/posts`);
+  const data = await axios.get(`${import.meta.env.VITE_URI}/posts`);
   return data;
 };
 
 export const getuser = async (): Promise<unknown> => {
-  const data = await axios.get(baseURL);
+  const data = await axios.get(import.meta.env.VITE_URI);
   return data;
 };
 
 export const deletePost = async (
   id: AxiosRequestConfig<unknown> | undefined
 ) => {
-  const data = await axios.delete(baseURL, id);
+  const data = await axios.delete(import.meta.env.VITE_URI, id);
   return data;
 };
 
 export const update = async (id: unknown) => {
-  const data = await axios.put(baseURL, id);
+  const data = await axios.put(import.meta.env.VITE_URI, id);
 
   return data;
 };
